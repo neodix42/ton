756aa66ec4f63b24881e201ef5e414bab016ba29
neodiX <neodix@ton.org>
2025-03-25 11:27:36 +0400

    with gh cache

--- assembly/native/build-ubuntu-appimages.sh
+++ assembly/native/build-ubuntu-appimages.sh
@@ -113,6 +113,5 @@ fi
 
 if [ "$with_tests" = true ]; then
   cd build
-#  ctest --output-on-failure -E "test-catchain|test-actors|test-smartcont|test-adnl|test-validator-session-state|test-dht|test-rldp"
   ctest --output-on-failure --timeout 1800
 fi
